c9357ae81eaddf0a35d16a82c46fc3d25e75eb0a
ssharma <ssharma@ebates.com>
2018-10-16 14:58:43 -0700

    Add contact properties to Owner Closes #24

--- pet-clinic-data/src/main/java/com/sanyukt2505/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/sanyukt2505/petclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(final String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;

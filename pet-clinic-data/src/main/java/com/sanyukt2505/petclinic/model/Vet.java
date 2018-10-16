ccfc3806b741ef20d6e2ad1818aaa8a4bfbdbd56
ssharma <ssharma@ebates.com>
2018-10-16 14:52:14 -0700

    Create Vet Speciality Entity and associate to Vet  Closes #23

--- pet-clinic-data/src/main/java/com/sanyukt2505/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/sanyukt2505/petclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.sanyukt2505.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialitites;
+
+    public Set<Speciality> getSpecialitites() {
+        return specialitites;
+    }
+
+    public void setSpecialitites(final Set<Speciality> specialitites) {
+        this.specialitites = specialitites;
+    }
 }

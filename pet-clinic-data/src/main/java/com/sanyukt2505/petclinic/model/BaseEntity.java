dd6540b419dd0014b758ca2624f60f37dc85f339
ssharma <ssharma@ebates.com>
2018-10-28 20:29:27 -0700

    Implement BaseEntity (JPA) Closes #30

--- pet-clinic-data/src/main/java/com/sanyukt2505/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sanyukt2505/petclinic/model/BaseEntity.java
@@ -2,9 +2,15 @@ package com.sanyukt2505.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    private Long id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
 
     public Long getId() {
         return id;
